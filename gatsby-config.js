cee9ec91676271c898c6b5ceb0ec43480bf9528a
Rolando Duma <ziogiugno@gmail.com>
2020-04-11 03:28:17 +0200

    migrate from starter blog to gatsby themes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Il blog del primate`,
     author: `Rolando Duma`,
+    shortBio: `Java developer, mobile and web`,
     description: `Qualche considerazione primordiale sul mondo dell'informatica che mi riguarda.`,
     siteUrl: `https://ziogiugno.github.io/`,
     social: [
@@ -17,70 +18,21 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "@joshkennedy00/gatsby-theme-blog-remix",
       options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`
+        basePath: "/"
       }
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`
-      }
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        pedantic: false,
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590
-            }
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`
-            }
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`
-        ]
-      }
-    },
-    `gatsby-plugin-sass`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `UA-139425776-1`
-      }
-    },
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Feed del Blog del Primate`,
-        short_name: `Primate Feed`,
+        name: `Gatsby Theme Remix`,
+        short_name: `remix`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`
-      }
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`
+        background_color: `#F86278`,
+        theme_color: `#F86278`,
+        display: `standalone`,
+        icon: `./content/assets/logo.svg` // This path is relative to the root of the site.
       }
     }
   ]
